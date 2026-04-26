b362f861a41bc1e985bbbcb35c5248a7169836ca
Yll Veliu <yll.veliu1@gmail.com>
2026-04-26 15:49:23 +0200

    Allow deployed frontend origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,9 +17,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "https://localhost:5173",
-    ],
+    "http://localhost:5173",
+    "https://project-management-system-ochre-ten.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
